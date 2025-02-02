4e6b630ddc5d30f9f73285ac7eea8f3223a4db35
Joachim-barre <joachim.barre22@laposte.net>
2025-02-02 20:57:07 +0100

    added source file opening

--- src/main.asm
+++ src/main.asm
@@ -3,15 +3,19 @@ section .note.GNU-stack noalloc noexec nowrite progbits
 section .data
 	arg_error db "This program only takes one argument", 10, 0
 	usage db "Usage : prog [source]", 10, 0
+	open_error db "Error : failed to open source file", 0
+	open_mode db "r", 0
 
 section .text
 	global main
 	extern printf
+	extern perror
+	extern fopen
 
 main: ; (int argc, char** argv) -> int
 	push rbp
 	mov rbp, rsp
-	sub rsp, 4
+	sub rsp, 12
 
 	xor edx, edx
 	mov dword [rbp-4], edx
@@ -21,7 +25,21 @@ main: ; (int argc, char** argv) -> int
 	cmp rdi, 2
 	jne .arg_error
 	
-	jmp .exit
+	mov rdi, [rsi+8]
+	lea rsi, [open_mode]
+	call fopen
+
+	test rax, rax
+	jz .open_error
+
+	mov qword [rbp-12], rax ; save file pointer
+	.open_error:
+		lea rdi, [open_error]
+		call perror
+
+		mov edx, 1
+		mov dword [rbp-4], edx
+		jmp .exit
 
 	.arg_error:
 		lea rdi, [arg_error]
