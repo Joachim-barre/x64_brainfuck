6476730672edd5c8707ea60b3486c94d25e38e13
Joachim-barre <joachim.barre22@laposte.net>
2025-02-02 21:28:56 +0100

    added basic file reading loop

--- src/main.asm
+++ src/main.asm
@@ -5,17 +5,22 @@ section .data
 	usage db "Usage : prog [source]", 10, 0
 	open_error db "Error : failed to open source file", 0
 	open_mode db "r", 0
+	io_error db "IO error", 0
 
 section .text
 	global main
 	extern printf
 	extern perror
 	extern fopen
+	extern fgetc
+	extern isspace
+	extern ferror
+	extern putchar
 
 main: ; (int argc, char** argv) -> int
 	push rbp
 	mov rbp, rsp
-	sub rsp, 12
+	sub rsp, 16
 
 	xor edx, edx
 	mov dword [rbp-4], edx
@@ -33,6 +38,48 @@ main: ; (int argc, char** argv) -> int
 	jz .open_error
 
 	mov qword [rbp-12], rax ; save file pointer
+
+	.exec_loop:
+		mov rdi, qword [rbp-12]
+		call fgetc
+
+		test eax, eax
+		js .eof
+
+		mov dword [rbp-16], eax
+		mov edi, dword [rbp-16]
+		call isspace
+		
+		test eax, eax
+		jnz .exec_loop
+
+		mov edi, dword [rbp-16]
+		call putchar
+
+		test eax, eax
+		js .io_error
+
+		jmp .exec_loop	
+
+	.eof:
+		mov rdi, qword [rbp-12]
+		call ferror
+
+		test eax, eax
+		jnz .io_error
+
+		mov edx, 0
+		mov dword [rbp-4], edx
+		jmp .exit
+
+	.io_error:
+		lea rdi, [io_error]
+		call perror
+
+		mov edx, 1
+		mov dword [rbp-4], edx
+		jmp .exit
+
 	.open_error:
 		lea rdi, [open_error]
 		call perror
